001db15a5c8df4e6be03fdcb9949d13362737152
Christoph Gutmann <christoph.gutmann@gmx.ch>
2025-10-09 19:32:39 +0200

    delete works

--- delete-stack.sh
+++ delete-stack.sh
@@ -1,19 +1,40 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-stackname=$(tomlq -r '.default.global.parameters.stack_name' samconfig.toml)
-region=$(tomlq -r '.default.deploy.parameters.region' samconfig.toml)
+STACK_NAME=$(tomlq -r '.default.global.parameters.stack_name' samconfig.toml)
+REGION=$(tomlq -r '.default.deploy.parameters.region' samconfig.toml)
+REPO_NAME=$(cat .resourceids-${STACK_NAME}/ExampleTaskRepository.txt)
 
 echo "You are about to delete the CloudFormation stack:"
-echo "  Name   : ${stackname}"
-echo "  Region : ${region}"
+echo "  Name   : ${STACK_NAME}"
+echo "  Region : ${REGION}"
 echo
 read -r -p "Are you sure? (y/N): " confirm
 
 if [[ "${confirm}" =~ ^[Yy]$ ]]; then
-  echo "Deleting stack ${stackname} in region ${region}..."
-  aws cloudformation delete-stack --region "${region}" --stack-name "${stackname}"
-  echo "Delete command submitted. Watch it go away by opening the AWS console https://${region}.console.aws.amazon.com/cloudformation/home"
+
+  echo "Deleting all images from ECR repository: $REPO_NAME in region $REGION"
+  read -p "Are you sure you want to delete ALL images from this repository? Type 'yes' to continue: " CONFIRM
+  if [ "$CONFIRM" != "yes" ]; then
+    echo "Aborted. No images deleted."
+    exit 1
+  fi
+
+  # List all image digests
+  IMAGE_IDS=$(aws ecr list-images --region "$REGION" --repository-name "$REPO_NAME" --query 'imageIds[*]' --output json)
+
+  if [ "$IMAGE_IDS" != "[]" ]; then
+    aws ecr batch-delete-image --region "$REGION" --repository-name "$REPO_NAME" --image-ids "$IMAGE_IDS"
+    echo "All images deleted."
+  else
+    echo "No images found in repository."
+  fi
+
+
+  echo "Deleting stack ${STACK_NAME} in region ${REGION}..."
+  aws cloudformation delete-stack --region "${REGION}" --stack-name "${STACK_NAME}"
+  echo "Delete command submitted. Watch it go away by opening the AWS console:"
+  echo "https://${REGION}.console.aws.amazon.com/cloudformation/home"
 else
   echo "Aborted."
 fi
